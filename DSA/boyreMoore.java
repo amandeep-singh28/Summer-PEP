b16232ebf43b00fe181d721d4d0baa221b724ef4
Amandeep Singh <amandeep87557@gmail.com>
2026-06-24 12:12:23 +0530

    Boyre Moore Algorithm

---
+++ DSA/boyreMoore.java
@@ -0,0 +1,15 @@
+public class boyreMoore {
+    public int majorityElement(int[] nums) {
+        int res = nums[0];
+        int count = 1;
+        for (int i = 1; i < nums.length; i++) {
+            if (res == nums[i]) count++;
+            else count--;
+            if (count == 0) {
+                res = nums[i];
+                count = 1;
+            }
+        }
+        return res;
+    }
+}
