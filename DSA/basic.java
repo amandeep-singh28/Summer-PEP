3c7ec61b02a4100c53f159a337506544d1fb92d0
Amandeep Singh <amandeep87557@gmail.com>
2026-06-24 21:56:48 +0530

    Updation

--- DSA/basic.java
+++ DSA/basic.java
@@ -1,5 +1,8 @@
 // import java.util.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class basic {
     public static void main(String[] args) {
         // int num1 = 1;
@@ -16,14 +19,28 @@ public class basic {
         //     }
         // }
 
-        String str = "abcda";
-        int[] freq = new int[26];
-        for (int i = 0; i < str.length(); i++) {
-            freq[(int)str.charAt(i) - 97]++;  // 97
+        // String str = "abcda";
+        // int[] freq = new int[26];
+        // for (int i = 0; i < str.length(); i++) {
+        //     freq[(int)str.charAt(i) - 97]++;  // 97
+        // }
+        // for (int i = 0; i < 26; i++) {
+        //     System.out.print(freq[i] + " ");
+        // }
+
+        int arr[] = {6, 7, 4, 5, 3, 1};
+        List<Integer> list = new ArrayList<>();
+        for (int i = 0; i < arr.length; i++) {
+            boolean flag = false;
+            for (int j = i + 1; j < arr.length; j++) {
+                if (arr[j] > arr[i]) {
+                    flag = true;
+                }
+                // else if (arr[j] > arr[i]) flag = false;
+            }
+            if (flag == false) list.add(arr[i]);
         }
-        for (int i = 0; i < 26; i++) {
-            System.out.print(freq[i] + " ");
-        }
+        System.out.println(list);
         
     }
 }
