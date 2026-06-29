81a16f882fc59fe5abfb3205cda5311ae59f865c
Amandeep Singh <amandeep87557@gmail.com>
2026-06-29 15:42:52 +0530

    Updation

--- DSA/basic.java
+++ DSA/basic.java
@@ -1,7 +1,5 @@
-// import java.util.*;
+import java.util.*;
 
-import java.util.ArrayList;
-import java.util.List;
 
 public class basic {
     public static void main(String[] args) {
@@ -28,19 +26,41 @@ public class basic {
         //     System.out.print(freq[i] + " ");
         // }
 
-        int arr[] = {6, 7, 4, 5, 3, 1};
-        List<Integer> list = new ArrayList<>();
-        for (int i = 0; i < arr.length; i++) {
-            boolean flag = false;
-            for (int j = i + 1; j < arr.length; j++) {
-                if (arr[j] > arr[i]) {
-                    flag = true;
-                }
-                // else if (arr[j] > arr[i]) flag = false;
+        // int arr[] = {6, 7, 4, 5, 3, 1};
+        // List<Integer> list = new ArrayList<>();
+        // for (int i = 0; i < arr.length; i++) {
+        //     boolean flag = false;
+        //     for (int j = i + 1; j < arr.length; j++) {
+        //         if (arr[j] > arr[i]) {
+        //             flag = true;
+        //         }
+        //         // else if (arr[j] > arr[i]) flag = false;
+        //     }
+        //     if (flag == false) list.add(arr[i]);
+        // }
+        // System.out.println(list);
+
+        // int arr[] = {1, 2, 5, 9, 15, 18};
+        // int target = 15;
+        // int left = 0;
+        // int right = arr.length - 1;
+        // while (left <= right) {
+        //     int mid = (left + right) / 2;
+        //     if (arr[mid]== target) System.out.println("Element found");
+        //     else if (arr[mid] < target) left = mid + 1;
+        //     else right = mid - 1;
+        // }
+
+        String str = "abcAbc";
+        Map<Character, Integer> mp = new HashMap<>();
+        char[] arr = str.toCharArray(); // [a, b, c]
+        for (var e : arr) {
+            if (mp.containsKey(e) == false) {
+                mp.put(e, 1);
+            } else {
+                mp.put(e, mp.get(e) + 1);
             }
-            if (flag == false) list.add(arr[i]);
         }
-        System.out.println(list);
-        
+        System.out.println(mp);
     }
 }
